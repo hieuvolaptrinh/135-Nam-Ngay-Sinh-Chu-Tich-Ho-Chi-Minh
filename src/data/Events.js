68e64162e774dada709fc2d941dd28d0eccff9a8
hieuvolaptrinh <vndhieuak@gmail.com>
2025-05-09 13:28:05 +0700

    bug image sm

--- src/data/Events.js
+++ src/data/Events.js
@@ -114,6 +114,9 @@ export const hcmEvents = [
   },
 ];
 //  "Đoạn văn bản. <br />[img]/images/example.jpg|Chú thích cho hình ảnh[/img]<br />Đoạn văn bản tiếp theo. <br />[img]/images/example2.jpg[/img]<br />Đoạn văn bản cuối cùng.";
+
+/* <br /><br /> */
+
 export const giaiPhongEvents = [
   {
     id: 1,
@@ -122,23 +125,99 @@ export const giaiPhongEvents = [
     location: "Hội trường Thống Nhất & đường Lê Duẩn, Quận 1, TP.HCM",
     description:
       "Sự kiện trọng thể kỷ niệm 50 năm Giải phóng miền Nam với hơn 13.000 người tham gia diễu binh, diễu hành.",
-    details:
-      "Lễ diễu binh quy mô lớn với sự tham gia của các lực lượng vũ trang, công an, thanh niên, phụ nữ và đại diện kiều bào. Mở đầu bằng 21 loạt đại bác, tiếp theo là màn trình diễn không quân và đội hình diễu hành qua Hội trường Thống Nhất, tái hiện khí thế lịch sử ngày 30/4.",
-    image: "/images/DuyetBinh.jpg",
+    details: `Sáng 30/4, tại Thành phố Hồ Chí Minh rực rỡ tên vàng, Ban Chấp hành Trung ương Đảng, Quốc hội, Chủ tịch nước, Chính phủ, Ủy ban Trung ương Mặt trận Tổ quốc Việt Nam và Ủy ban nhân dân Thành phố Hồ Chí Minh đã tổ chức trọng thể Lễ kỷ niệm 50 năm Ngày Giải phóng miền Nam, thống nhất đất nước (30/4/1975-30/4/2025).
+
+Từ sáng sớm, thời tiết ở Thành phố Hồ Chí Minh rất đẹp, trong không khí tưng bừng, phấn khởi, rực rỡ cờ hoa kỷ niệm ngày lễ lớn, tại khu vực Quảng trường Thống Nhất (dọc tuyến đường Lê Duẩn, trước cổng Hội trường Thống Nhất), đông đảo cán bộ, chiến sỹ, đồng bào cùng các tầng lớp nhân dân Thành phố Hồ Chí Minh đã tề tựu chào mừng Lễ kỷ niệm.<br />[img]./images/GiaiPhong/DieuBinh/DB1.jpg|Mở đầu, Tổng Bí thư Tô Lâm đọc diễn văn tại Lễ kỷ niệm, diễu binh, diễu hành 50 năm Ngày Giải phóng miền nam, thống nhất đất nước (30/4/1975-30/4/2025).[/img]<br />
+Dự Lễ kỷ niệm có các đồng chí: Tổng Bí thư Tô Lâm; nguyên Tổng Bí thư Nông Đức Mạnh; Ủy viên Bộ Chính trị, Chủ tịch nước Lương Cường; các nguyên Ủy viên Bộ Chính trị, nguyên Chủ tịch nước: Nguyễn Minh Triết, Trương Tấn Sang; Ủy viên Bộ Chính trị, Thủ tướng Chính phủ Phạm Minh Chính; nguyên Ủy viên Bộ Chính trị, nguyên Thủ tướng Chính phủ Nguyễn Tấn Dũng; Ủy viên Bộ Chính trị, Chủ tịch Quốc hội Trần Thanh Mẫn; các nguyên Ủy viên Bộ Chính trị, nguyên Chủ tịch Quốc hội: Nguyễn Văn An, Nguyễn Thị Kim Ngân; Ủy viên Bộ Chính trị, Thường trực Ban Bí thư Trần Cẩm Tú, Trưởng Ban Chỉ đạo Trung ương kỷ niệm các ngày lễ lớn và sự kiện lịch sử quan trọng của đất nước năm 2025; các nguyên Ủy viên Bộ Chính trị, nguyên Thường trực Ban Bí thư: Phan Diễn, Trần Quốc Vượng; Ủy viên Bộ Chính trị, Bí thư Trung ương Đảng, Chủ tịch Ủy ban Trung ương Mặt trận Tổ quốc Việt Nam Đỗ Văn Chiến.
+
+Cùng dự Lễ kỷ niệm có các Ủy viên Bộ Chính trị, nguyên Ủy viên Bộ Chính trị; Bí thư Trung ương Đảng, nguyên Bí thư Trung ương Đảng; Ủy viên Trung ương Đảng, nguyên Ủy viên Trung ương Đảng; các đồng chí: Phó Chủ tịch nước, nguyên Phó Chủ tịch nước, Phó Thủ tướng Chính phủ, nguyên Phó Thủ tướng Chính phủ, Phó Chủ tịch Quốc hội, nguyên Phó Chủ tịch Quốc hội, thành viên Ban Chỉ đạo Trung ương kỷ niệm các ngày lễ lớn và sự kiện lịch sử quan trọng trong năm 2025; lãnh đạo các ban, bộ, ngành, đoàn thể Trung ương và Thành phố Hồ Chí Minh, lãnh đạo một số tỉnh, thành phố trực thuộc Trung ương; lão thành cách mạng, các Mẹ Việt Nam anh hùng, Anh hùng Lực lượng vũ trang nhân dân, Anh hùng Lao động, các tướng lĩnh trong lực lượng vũ trang nhân dân; đại diện các chiến sĩ, cựu chiến binh, cựu công an nhân dân, cựu thanh niên xung phong, cựu dân công hỏa tuyến, các lực lượng tham gia Chiến dịch Hồ Chí Minh lịch sử; đại diện các gia đình thương binh, liệt sĩ, gia đình có công với nước.
+<br />[img]./images/GiaiPhong/DieuBinh/DB2.jpg|Tổng Bí thư Tô Lâm thay mặt lãnh đạo Đảng, Nhà nước trao tặng danh hiệu “Anh hùng Lao động” cho Đảng bộ, Chính quyền và nhân dân Thành phố Hồ Chí Minh.[/img]<br />
+Đến dự Lễ kỷ niệm có các đoàn khách quốc tế cấp cao: Đoàn đại biểu cấp cao Đảng,
+ Nhà nước Cộng hòa Dân chủ Nhân dân Lào do đồng chí Thongloun Sisoulith, Tổng Bí thư Đảng Nhân dân Cách mạng Lào, 
+ Chủ tịch nước Cộng hòa Dân chủ Nhân dân Lào làm Trưởng đoàn; Đoàn đại biểu cấp cao của Vương quốc Campuchia do ngài
+  Samdech Techo Hun Sen, Chủ tịch Đảng Nhân dân Campuchia, Chủ tịch Thượng viện Vương quốc Campuchia làm Trưởng đoàn; Đoàn đại biểu 
+  Đảng, Nhà nước Cộng hòa Cuba do đồng chí Salvador Valdes Mesa, Phó Chủ tịch nước Cộng hòa Cuba làm Trưởng đoàn; Đoàn đại biểu Cộng hò
+  a Belarus do ngài Ipatau Vadzim, Phó Chủ tịch Hạ viện Cộng hòa Belarus làm Trưởng đoàn; Đoàn đại biểu của Đảng, Nhà nước Cộng hòa Nhân dân Trung Hoa do đồng
+   chí Bùi Kim Giai, Bộ trưởng Bộ các vấn đề quân nhân xuất ngũ làm Trưởng đoàn; các Bộ trưởng, Thứ trưởng Bộ Ngoại giao, Bộ Quốc phòng các nước, các chính đảng,
+    các tổ chức quốc tế, Đại sứ, Trưởng cơ quan đại diện, tùy viên quốc phòng của các nước tại Việt Nam và đông đảo bạn bè quốc tế từng ủng hộ Việt Nam trong sự 
+    nghiệp đấu tranh, giải phóng dân tộc và xây dựng đất nước. 
+    Mở đầu Lễ kỷ niệm là chương trình nghệ thuật chào mừng được dàn dựng công phu với chủ đề: “Rạng rỡ non sông Việt Nam” do Ban Tổ chức Kỷ niệm các ngày lễ lớn Thành phố Hồ Chí Minh chủ trì, phối hợp cùng Bộ Văn hóa, Thể thao và Du lịch, Bộ Quốc phòng, Bộ Công an thực hiện.
+Chương trình có màn biểu diễn trống hội với chủ đề: “Bản hùng ca toàn thắng”, do 1.000 diễn viên là học viên thuộc Đoàn Nghệ thuật trống hội Học viện Cảnh sát nhân dân - Bộ Công an trình diễn; màn xếp hình nghệ thuật với chủ đề: “Đất nước trọn niềm vui” do các nghệ sĩ, chiến sĩ Quân đội nhân dân Việt Nam biểu diễn; cùng nhiều tiết mục nghệ thuật tái hiện quá trình đấu tranh giành độc lập, xây dựng và phát triển đất nước hào hùng của dân tộc.<br /><br />
+Lễ kỷ niệm bắt đầu bằng nghi lễ chào cờ trong tiếng rền vang của 21 loạt đại bác do đội pháo lễ thực hiện tại bến Bạch Đằng, Quận 1, Thành phố Hồ Chí Minh.
+
+Tổng Bí thư Tô Lâm đọc diễn văn Kỷ niệm 50 năm Ngày Giải phóng miền Nam, thống nhất đất nước.
+
+Tiếp theo là phát biểu của đại diện cựu chiến binh, của đại diện thế hệ trẻ Việt Nam.
+
+Chương trình diễu binh, diễu hành do Bộ Quốc phòng chủ trì, bao gồm phần trình diễn của máy bay trực thăng và tiêm kích chiến đấu; chương trình diễu binh của 4 khối Nghi trượng, 36 khối diễu binh của Quân đội và Công an, 12 khối diễu hành và khối diễu binh của quân đội Trung Quốc, Lào và Campuchia. Đặc biệt, có 350 đại biểu là các Anh hùng Lực lượng vũ trang nhân dân, Anh hùng Lao động, nhân chứng lịch sử tiêu biểu sẽ tham gia trong lễ diễu hành trên xe ô tô buýt 2 tầng.
+
+Nhân dịp này, nhằm ghi nhận thành tích đặc biệt xuất sắc của Đảng bộ, chính quyền và nhân dân Thành phố Hồ Chí Minh, góp phần vào sự nghiệp xây dựng chủ nghĩa xã hội và bảo vệ Tổ quốc, Đảng, Nhà nước đã quyết định phong tặng danh hiệu Anh hùng Lao động tới Đảng bộ, chính quyền và nhân dân Thành phố Hồ Chí Minh.
+<br /><br /> 
+Chương trình được truyền hình trực tiếp trên VTV1 - Đài Truyền hình Việt Nam, Đài Truyền hình Thành phố Hồ Chí Minh và các đài phát thanh truyền hình địa phương. Thành phố Hồ Chí Minh đã lắp đặt 20 màn hình LED lớn trên các tuyến đường phố trung tâm, phục vụ người dân và du khách, phát trực tiếp các hoạt động của lễ kỷ niệm.
+Khi tiếng Quốc ca vang lên thiêng liêng giữa bầu trời xanh ngát phương Nam, 21 loạt đại bác từ bến Bạch Đằng rền vang như lời tuyên ngôn khẳng định sức mạnh và ý chí bất khuất của dân tộc Việt Nam. Sau bài diễn văn đầy xúc động và khí phách của Tổng Bí thư Tô Lâm, phần diễu binh – diễu hành long trọng được khởi đầu lúc 8 giờ 5 phút, dưới sự điều hành trực tiếp của Thượng tướng Nguyễn Văn Nghĩa – Phó Tổng tham mưu trưởng Quân đội nhân dân Việt Nam.
+
+<br/ > [img]./images/GiaiPhong/DieuBinh/DB3.jpg|Chính thức vào 8 giờ 5 phút, Thượng tướng Nguyễn Văn Nghĩa, Phó Tổng tham mưu trưởng Quân đội nhân dân Việt Nam điều hành chương trình diễu binh, diễu hành kỷ niệm 50 năm "Ngày Giải phóng miền nam, thống nhất đất nước".[/img] <br />
+Bầu trời rực rỡ thêm phần hùng vĩ với màn trình diễn hoành tráng của các phi đội trực thăng và tiêm kích chiến đấu, mở đầu cho đại lễ của khí thế và sức mạnh. Tiếp đến, các khối diễu binh lần lượt bước qua lễ đài theo đội hình nghiêm trang, thể hiện bản lĩnh, kỷ luật và uy phong của các lực lượng vũ trang nhân dân.
+
+Dẫn đầu là khối Nghi trượng và Quân kỳ Quyết thắng – biểu tượng thiêng liêng cho hồn thiêng sông núi. Theo sau là các khối sĩ quan Lục quân, Hải quân, Không quân, Biên phòng, Cảnh sát biển, đặc công, pháo binh, phòng không, tăng thiết giáp, công binh, thông tin, hóa học, tác chiến điện tử, quân y, hậu cần…
+<br /> [img]./images/GiaiPhong/DieuBinh/DB5.jpg|Khối sĩ quan Biên phòng.[/img] <br /> 
+<br /> [img]./images/GiaiPhong/DieuBinh/DB6.jpg|Khối sĩ quan Cảnh sát biển.[/img]<br /> 
+<br /> [img]./images/GiaiPhong/DieuBinh/DB7.jpg|Khối nữ Quân y.[/img]<br /> 
+
+
+Khối nữ quân nhân bước đi dứt khoát, đều tắp, mạnh mẽ và thanh lịch, như những đóa hoa thép của Quân đội nhân dân Việt Nam. Khối công an nhân dân diễu hành với khí thế hiên ngang, đại diện cho lực lượng giữ gìn trật tự an ninh quốc gia, bảo vệ cuộc sống bình yên cho nhân dân. Các khối lực lượng vũ trang ba nước bạn: Lào, Campuchia, Trung Quốc – góp phần khẳng định tình đoàn kết quốc tế bền vững và sâu sắc.
+Một hình ảnh xúc động nhất là sự hiện diện của 350 đại biểu là các Anh hùng Lực lượng vũ trang nhân dân, Anh hùng Lao động, nhân chứng lịch sử tiêu biểu – những người đã làm nên chiến thắng – diễu hành trên xe buýt hai tầng, trong tiếng vỗ tay không dứt của đồng bào hai bên đường.
+<br /><br /> 
+12 khối diễu hành của các lực lượng dân quân tự vệ, thanh niên xung phong, học sinh – sinh viên, công nhân, trí thức, nông dân, đồng bào các dân tộc, đại diện các tầng lớp nhân dân… đồng loạt thể hiện tinh thần đại đoàn kết toàn dân tộc, quyết tâm giữ vững thành quả cách mạng, xây dựng và bảo vệ Tổ quốc.
+<br /><br /> 
+Cả quảng trường như lặng đi khi danh hiệu "Anh hùng Lao động" được trao cho Đảng bộ, chính quyền và nhân dân Thành phố Hồ Chí Minh – vùng đất thành đồng, trung dũng kiên cường, năng động sáng tạo, góp phần xứng đáng vào sự nghiệp xây dựng và bảo vệ Tổ quốc.
+
+
+
+
+
+    `,
+    image: "./images/GiaiPhong/DieuBinh/DuyetBinh.jpg",
     video: "https://www.youtube.com/watch?v=V2cWKa1M98w",
   },
   {
     id: 2,
-    title: "Lễ hội 'Sắc màu Thành phố Bác'",
+    title: `"Lễ hội 'Sắc màu Thành phố Bác"`,
     date: "19/04 – 30/04/2025",
     location: "Mặt tiền UBND TP.HCM, Quận 1",
     description:
-      "Lễ hội ánh sáng kết hợp trình chiếu 3D Mapping độc đáo trên mặt tiền trụ sở UBND TP.HCM.",
-    details:
-      "Trình chiếu nghệ thuật 3D Mapping kết hợp âm thanh, ánh sáng và nhạc giao hưởng – hợp xướng, tái hiện những dấu ấn hào hùng của dân tộc và sự phát triển hiện đại của TP.HCM. Đây là điểm nhấn nghệ thuật thu hút hàng vạn lượt người xem trong suốt thời gian lễ hội.",
+      " Đây là đợt lễ hội lớn nhằm hưởng ứng kỷ niệm 50 năm đất nước thống nhất (30.4.1975 - 30.4.2025) với nhiều hoạt động được tổ chức",
+    details: `
+    Đây là sự kiện mở màn cho đợt lễ hội lớn nhằm hưởng ứng Kỷ niệm 50 năm Ngày Giải phóng miền Nam, thống nhất đất nước (30/4/1975 – 30/4/2025)
+
+Tham dự chương trình có Phó Chủ tịch UBND TP Hồ Chí Minh Trần Thị Diệu Thúy, Chủ nhiệm Ủy ban Kiểm tra Thành ủy TP Hồ Chí Minh Trần Kim Yến; nguyên Trưởng ban Tuyên giáo Thành ủy TP Hồ Chí Minh Phan Nguyễn Như Khuê; bà Emmanuelle Pavillon-Grosser - Tổng lãnh sự Pháp tại TP Hồ Chí Minh.
+      
+<br /> [img]./images/GiaiPhong/SacMauTPbac/SM1.jpg|Phó Chủ tịch UBND TP Hồ Chí Minh - Trần Thị Diệu Thúy - phát biểu tại lễ khai mạc.[/img]<br /> 
+
+Phát biểu tại lễ khai mạc, Phó Chủ tịch UBND TP Hồ Chí Minh - Trần Thị Diệu Thúy - cho rằng, chuỗi hoạt động Sắc màu Thành phố Bác không chỉ là sự kiện văn hóa nghệ thuật đơn thuần mà còn mang ý nghĩa chính trị, lịch sử sâu sắc. Tròn 50 năm sau ngày Giải phóng miền Nam, thống nhất đất nước, TP Hồ Chí Minh -  thành phố mang tên Bác, đã không ngừng nỗ lực vươn lên, từng bước khẳng định vai trò là trung tâm kinh tế, văn hóa, khoa học, giáo dục, đổi mới sáng tạo và hội nhập quốc tế của cả nước.
+     
+Đây là dịp để người dân thành phố và cả nước cùng nhau tôn vinh những giá trị của độc lập, tự do, tri ân những thế hệ cha anh đã ngã xuống vì sự nghiệp giải phóng dân tộc, đồng thời quảng bá hình ảnh một thành phố hiện đại, sáng tạo, nghĩa tình và hội nhập đến bạn bè trong nước và quốc tế", bà Trần Thị Diệu Thúy nhấn mạnh.
+
+Phần trình diễn 3D Mapping quốc tế mở đầu lễ hội, với sự tham gia của 3 đội sáng tạo đến từ Pháp, Bỉ, Singapore. Mỗi đội mang đến một sắc màu riêng trong cách kể chuyện bằng ánh sáng, từ những biểu tượng văn hóa, công nghệ đến cái nhìn đương đại về TP Hồ Chí Minh. Ấn tượng nhất trong đêm khai mạc là màn trình diễn của đội chủ nhà Việt Nam, đưa khán giả du ngoạn dọc theo mảnh đất hình chữ S, tái hiện lại những thời khắc lịch sử hào hùng của dân tộc.
+
+<br /> [img]./images/GiaiPhong/SacMauTPbac/SM2.jpg|Đại tiệc 3D Mapping mãn nhãn.[/img]<br /> 
+Chương trình hòa nhạc giao hưởng, hợp xướng kết hợp với công nghệ trình diễn Mapping Visual Art cũng là sáng tạo mới, phát huy sự giao thoa, tương tác của của loại hình nghệ thuật hàn lâm và đương đại cũng đã mang đến nhiều cảm xúc cho người xem.
+<br /> [img]./images/GiaiPhong/SacMauTPbac/SM3.jpg|Chương trình nhạc giao hưởng, hợp xướng kết hợp trình diễn Mapping Visual Art mang lại nhiều cảm xúc cho khán giả.[/img]<br /> 
+Cùng lúc, các khu vực như công viên Bạch Đằng, sân khấu nổi trên sông Sài Gòn, phố đi bộ Nguyễn Huệ trở thành không gian lễ hội với các hoạt động múa lân – sư – rồng, tuồng, đờn ca tài tử, võ thuật, thể thao đường phố… đã tạo ra một không khí lễ hội rộn ràng, náo nhiệt thu hút rất đông người dân và du khách tham gia.
+
+Điểm nhấn của lễ khai mạc là những màn trình diễn xếp hình bằng ánh sáng bởi công nghệ thiết bị bay không người lái (drone) và pháo hoa nghệ thuật diễn ra tại Công viên Bờ sông Sài Gòn, thắp sáng bầu trời TP Hồ Chí Minh.
+<br /> [img]./images/GiaiPhong/SacMauTPBac/SM4.jpg|Màn pháo hoa rực sáng trên bầu trời TP Hồ Chí Minh.[/img]<br /> 
+
+Sau đêm khai mạc, các đêm 26/4/2025 và 29/4/2025 cũng sẽ tiếp tục diễn ra các hoạt động văn hóa, thể thao, công nghệ trình diễn sôi nổi. Đêm 30/4/2025, với chủ đề Ngày hội thống nhất non sông hứa hẹn một đêm bùng nổ với các chương trình biểu diễn nhạc kèn, diễu hành kỵ binh, nghệ thuật cộng đồng và đặc biệt là màn trình diễn xếp hình bằng ánh sáng bởi công nghệ thiết bị bay không người lái dự kiến lập kỷ lục thế giới với 10.500 thiết bị (drone).
+
+      Bên cạnh đó, các hoạt động triển lãm chuyên đề, chiếu phim công cộng trên tuyến phố đi bộ cũng góp phần lan tỏa thông điệp yêu nước, tôn vinh giá trị lịch sử, văn hóa, truyền thống cách mạng, tình yêu biển đảo và giới thiệu những thành tựu đáng tự hào trong tiến trình phát triển của thành phố.
+
+Chuỗi hoạt động Sắc màu Thành phố Bác là dịp để TP Hồ Chí Minh tự hào giới thiệu đến bạn bè trong nước và quốc tế một đô thị năng động, sáng tạo, đầy bản sắc - nơi hội tụ tinh thần dân tộc và khát vọng tương lai.
+      `,
     image: "/images/SacMauThanhPhoBac.jpg",
-    video: "https://www.youtube.com/watch?v=sacmau_example",
+    video: "https://www.youtube.com/watch?v=SpU4HFg5YWU",
   },
+
   {
     id: 3,
     title: "Triển lãm điện ảnh 'Âm vang đại thắng mùa Xuân 1975'",
@@ -159,7 +238,7 @@ export const giaiPhongEvents = [
     description:
       "Đêm nghệ thuật đặc biệt với sự tham dự của lãnh đạo Đảng và Nhà nước.",
     details:
-      "Chương trình nghệ thuật lớn với các tiết mục tái hiện hành trình thống nhất đất nước và các thành tựu sau 50 năm. Với sự tham gia của nhiều nghệ sĩ tên tuổi và lãnh đạo cấp cao, đây là sự kiện trọng điểm của chuỗi kỷ niệm. <br />[img]/images/MuaXuanThongNhat3.jpg|Các tiết mục nghệ thuật đặc sắc trong chương trình[/img]",
+      "Chương trình nghệ thuật lớn với các tiết mục tái hiện hành trình thống nhất đất nước và các thành tựu sau 50 năm. Với sự tham gia của nhiều nghệ sĩ tên tuổi và lãnh đạo cấp cao, đây là sự kiện trọng điểm của chuỗi kỷ niệm. <br />[img]/images/MuaXuanThongNhat.jpg|Các tiết mục nghệ thuật đặc sắc trong chương trình[/img]",
     image: "/images/MuaXuanThongNhat.jpg",
     video: "https://www.youtube.com/watch?v=muaxuan_example",
   },
