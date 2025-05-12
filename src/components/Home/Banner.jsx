927307e34a4da8333005d746381fd9109a43a083
hieuvolaptrinh <vndhieuak@gmail.com>
2025-05-13 01:20:45 +0700

    change

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -3,7 +3,11 @@ import { motion } from "framer-motion";
 const Banner = () => {
   return (
     <motion.div>
-      <img className="overlay  w-100 h-80" src="./images/Banner.gif" alt="" />
+      <img
+        className="overlay  w-100 h-80"
+        src="./images/BannerVip.gif"
+        alt=""
+      />
     </motion.div>
   );
 };
